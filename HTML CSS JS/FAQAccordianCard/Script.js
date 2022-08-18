3cdd8a934312147d59a2412263eddf6d7b13afca
somidh roy <somidhroy@gmail.com>
2022-08-18 22:12:09 +0530

    files added

---
+++ HTML CSS JS/FAQAccordianCard/Script.js
@@ -0,0 +1,8 @@
+const arrow = document.querySelector('.fa-solid')
+const questions = document.getElementsByTagName("h3")
+const answers = document.getElementsByTagName("p")
+
+
+arrow.addEventListener('click', () => {
+    
+})
