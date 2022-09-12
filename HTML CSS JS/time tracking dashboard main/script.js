37a00c67bcce1585e92e461ef1c90c529436f8bf
somidh roy <somidhroy@gmail.com>
2022-09-12 18:14:55 +0530

    files added

--- HTML CSS JS/time tracking dashboard main/script.js
+++ HTML CSS JS/time tracking dashboard main/script.js
@@ -5,43 +5,59 @@ const hrs = document.querySelectorAll('.hrs')
 const lastWeekHrs = document.querySelectorAll('.last-week-hrs')
 const activity = document.querySelectorAll('.activity')
 
+const dailyData = () => {
+    fetch("./data.json").then((res) => res.json()).then((data) => {
+        hrs.forEach((e, i) => {
+            hrs[i].innerHTML = data[i].timeframes.daily.current + "hrs"
+            lastWeekHrs[i].innerHTML = `Last Week - ${
+                data[i].timeframes.daily.previous
+            }hrs`
+        })
+    })
+} 
+dailyData()
+
+const weeklyData = () => {
+    fetch("./data.json").then((res) => res.json()).then((data) => {
+        hrs.forEach((e, i) => {
+            hrs[i].innerHTML = data[i].timeframes.weekly.current + "hrs"
+            lastWeekHrs[i].innerHTML = `Last Week - ${
+                data[i].timeframes.weekly.previous
+            }hrs`
+
+        })
+    })
+}
+const monthlyData = () => {
+    fetch("./data.json").then((res) => res.json()).then((data) => {
+        hrs.forEach((e, i) => {
+            hrs[i].innerHTML = data[i].timeframes.monthly.current + "hrs"
+            lastWeekHrs[i].innerHTML = `Last Week - ${
+                data[i].timeframes.monthly.previous
+            }hrs`
+
+        })
+    })
+} 
 
 
 dailyEl.addEventListener('click', () => {
     dailyEl.classList.add('active')
-
-    if(weeklyEl.classList.contains('active')){
-        weeklyEl.classList.remove('active')
-    }
-    if(monthlyEl.classList.contains('active')){
-        monthlyEl.classList.remove('active')
-    }
+    weeklyEl.classList.remove('active')
+    monthlyEl.classList.remove('active')
+    dailyData()
 })
 weeklyEl.addEventListener('click', () => {
     weeklyEl.classList.add('active')
-    if(dailyEl.classList.contains('active')){
-        dailyEl.classList.remove('active')
-    }
-    if(monthlyEl.classList.contains('active')){
-        monthlyEl.classList.remove('active')
-    }
+    dailyEl.classList.remove('active')
+    monthlyEl.classList.remove('active')
+    weeklyData()
+
 })
 monthlyEl.addEventListener('click', () => {
     monthlyEl.classList.add('active')
+    dailyEl.classList.remove('active')
+    weeklyEl.classList.remove('active')
+    monthlyData()
 
-    if(dailyEl.classList.contains('active')){
-        dailyEl.classList.remove('active')
-    }
-    if(weeklyEl.classList.contains('active')){
-        weeklyEl.classList.remove('active')
-    }
 })
-
-
-fetch('./data.json')
-.then((res) => res.json())
-.then((data) => {
-    console.log(data)
-   
-})
-
